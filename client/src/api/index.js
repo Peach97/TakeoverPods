e28387e05464cc6d541a5debb5372350a5249ebb
Cam <90569344+Peach97@users.noreply.github.com>
2022-10-20 23:19:42 -0400

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://takeover-pods.herokuapp.com" });
 const SPOTIFY = axios.create({
   baseURL: "http://accounts.spotify.com/authorize",
 });
